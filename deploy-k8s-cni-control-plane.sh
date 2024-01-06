25f59733518116858530f811a85e3a045f1a6ffe
Dong Ahn <donga@nvidia.com>
2024-01-05 21:38:01 -0800

    scripts: k8s cluster installer

---
+++ deploy-k8s-cni-control-plane.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+
+
---
+++ deploy-k8s-control-plane.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl get nodes
+
+
